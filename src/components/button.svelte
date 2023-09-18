73d7377edd5a196f61144ca21c50de6fcdddae9d
Humboorgir <hardcraftap@gmail.com>
2023-09-18 22:16:51 +0330

    Create Button component

---
+++ src/components/button.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { twMerge } from 'tailwind-merge'
+
+    const classes = "bg-neutral-700 rounded-md py-2 px-3 text-slate-200"
+    
+	export let className:string = "";
+</script>
+
+<button class={twMerge(classes, className)}><slot/></button>
