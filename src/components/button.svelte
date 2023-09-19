562fbd193ee770ea8870ab81b81f4a818d0ba651
Humboorgir <hardcraftap@gmail.com>
2023-09-19 17:04:59 +0330

    Add onClick handler to Button component

--- src/components/button.svelte
+++ src/components/button.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+  import type { MouseEventHandler } from 'svelte/elements';
     import { twMerge } from 'tailwind-merge'
 
     const classes = "bg-neutral-700 rounded-md py-2 px-3 text-slate-200"
     
 	export let className:string = "";
+    export let onClick:MouseEventHandler<HTMLButtonElement> = () => null;
 </script>
 
-<button class={twMerge(classes, className)}><slot/></button>
+<button class={twMerge(classes, className)} on:click={onClick}><slot/></button>
