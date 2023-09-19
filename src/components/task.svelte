12d6d66ec5ea8578994e793c7d56911aa31804aa
Humboorgir <hardcraftap@gmail.com>
2023-09-19 17:05:19 +0330

    Display formatted time in Task component

--- src/components/task.svelte
+++ src/components/task.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
     export let title:string;
     export let description:string;
-    export let time:string;
+    export let time:string|Date;
+
+//  formatting the time 
+const timeDate = new Date(time);
+const hour = timeDate.getHours();
+const minutes = timeDate.getMinutes();
+const paddedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+const hour12 = hour % 12 || 12;
+const amPm = hour < 12 ? 'AM' : 'PM';
+
+const timeFormatted = `${hour12}:${paddedMinutes} ${amPm}`;
 </script>
 
 <div class="text-neutral-200 flex items-start bg-[#3b3b3b] rounded-md p-2 mb-1.5">
@@ -9,6 +19,6 @@
     <div class="flex flex-col">
       <h2 class="text-lg text-neutral-200">{title}</h2>
       <p class="text-xs text-neutral-400 pr-2 mb-2">{description}</p>
-      <p class="text-xs text-neutral-500 self-end">{time}</p>
+      <p class="text-xs text-neutral-500 self-end">{timeFormatted}</p>
     </div>
   </div>
