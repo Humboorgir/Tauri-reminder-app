903becafcb3265c48e4c1135087a5f838192a5a0
Humboorgir <hardcraftap@gmail.com>
2023-09-20 22:47:57 +0330

    Create Input component

---
+++ src/components/input.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { HTMLInputTypeAttribute } from "svelte/elements";
+  export let placeholder: string;
+  export let name: string;
+  export let type: HTMLInputTypeAttribute = "text";
+</script>
+
+<input
+  class="rounded-md py-1 px-2 bg-transparent text-neutral-400 border-2 border-neutral-500
+focus:border-neutral-300 focus:text-neutral-200 outline-none transition-all"
+  {placeholder}
+  {type}
+  {name}
+/>
