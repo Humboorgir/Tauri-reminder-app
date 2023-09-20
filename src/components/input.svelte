9c95bad6dae458564b34555d37489211917b2bca
Humboorgir <hardcraftap@gmail.com>
2023-09-20 23:59:01 +0330

    Add optional required prop to Input component

--- src/components/input.svelte
+++ src/components/input.svelte
@@ -3,11 +3,13 @@
   export let placeholder: string;
   export let name: string;
   export let type: HTMLInputTypeAttribute = "text";
+  export let required: boolean = false;
 </script>
 
 <input
   class="rounded-md py-1 px-2 bg-transparent text-neutral-400 border-2 border-neutral-500
 focus:border-neutral-300 focus:text-neutral-200 outline-none transition-all"
+  {required}
   {placeholder}
   {type}
   {name}
