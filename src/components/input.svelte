02181233e705f96ab4ce42f6eab4e95dfb877513
Humboorgir <hardcraftap@gmail.com>
2023-09-21 00:08:52 +0330

    Add optional className prop to Input component

--- src/components/input.svelte
+++ src/components/input.svelte
@@ -1,16 +1,13 @@
 <script lang="ts">
   import type { HTMLInputTypeAttribute } from "svelte/elements";
+  import { twMerge } from "tailwind-merge";
   export let placeholder: string;
   export let name: string;
   export let type: HTMLInputTypeAttribute = "text";
   export let required: boolean = false;
+  export let className: string = "";
+  let classes = `rounded-md py-1 px-2 bg-transparent text-neutral-400 border-2 border-neutral-500
+     focus:border-neutral-300 focus:text-neutral-200 outline-none transition-all`;
 </script>
 
-<input
-  class="rounded-md py-1 px-2 bg-transparent text-neutral-400 border-2 border-neutral-500
-focus:border-neutral-300 focus:text-neutral-200 outline-none transition-all"
-  {required}
-  {placeholder}
-  {type}
-  {name}
-/>
+<input class={twMerge(classes, className)} {required} {placeholder} {type} {name} />
