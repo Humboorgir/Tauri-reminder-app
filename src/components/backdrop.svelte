e2a7d3fc44098c0773ec7b11393eb9a36d8d0b91
Humboorgir <hardcraftap@gmail.com>
2023-09-20 22:48:10 +0330

    Create Backdrop component

---
+++ src/components/backdrop.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { MouseEventHandler } from "svelte/elements";
+
+  export let closeModal: MouseEventHandler<HTMLDivElement>;
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div
+  on:click={closeModal}
+  class="absolute top-0 left-0 h-full w-full bg-black/30 flex items-center justify-center"
+>
+  <slot />
+</div>
