71d5eb1c127c74ba87991cba91d386510f2f5127
Humboorgir <hardcraftap@gmail.com>
2023-09-21 21:05:13 +0330

    Animate button when hovered and active

--- src/components/button.svelte
+++ src/components/button.svelte
@@ -2,7 +2,8 @@
   import type { MouseEventHandler } from "svelte/elements";
   import { twMerge } from "tailwind-merge";
 
-  const classes = "bg-neutral-700 rounded-md py-2 px-3 text-slate-200";
+  const classes =
+    "bg-neutral-700 rounded-md py-2 px-3 text-slate-200 hover:bg-opacity-75 active:bg-opacity-100";
 
   export let className: string = "";
   export let onClick: MouseEventHandler<HTMLButtonElement> = () => null;
