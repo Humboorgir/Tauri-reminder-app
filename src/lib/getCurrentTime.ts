772f3e22083fb5ff4ab1de78f6e62639fa26390e
Humboorgir <hardcraftap@gmail.com>
2023-09-25 20:50:43 +0330

    getCurrentTime(): Implement optional 'includeSeconds' prop

--- src/lib/getCurrentTime.ts
+++ src/lib/getCurrentTime.ts
@@ -1,8 +1,10 @@
-export default function getCurrentTime() {
+export default function getCurrentTime({ includeSeconds } = { includeSeconds: false }) {
   // returns the current time in the following format: 'Hour:Minute'
+  // includes seconds if includeSeconds is true
   let currentDate = new Date();
   let minute = currentDate.getMinutes() < 10 ? `0${currentDate.getMinutes()}` : currentDate.getMinutes();
   let hour = currentDate.getHours() < 10 ? `0${currentDate.getHours()}` : currentDate.getHours();
-  let currentTime = `${hour}:${minute}`;
+  let second = currentDate.getSeconds() < 10 ? `0${currentDate.getSeconds()}` : currentDate.getSeconds();
+  const currentTime = includeSeconds ? `${hour}:${minute}:${second}` : `${hour}:${minute}`;
   return currentTime;
 }
